58b4d0da14b3f1f03015dc74afac487979ed154c
Arnold <wangych_qd@163.com>
2017-01-08 17:22:31 +0800

    1st revise

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 #conding=utf8
+
+import requests
+import json
+
+
+s = requests.session()
+res=s.post('http://www.qingdaoport.net/user/login',json.load(file("account.json")));
+#换成抓取的地址
+r=s.get('http://www.qingdaoport.net/ywzx/zhcx/uni_cbklbcx.dw');
+print r.url
+#print r.text
+print(r.text, '\n{}\n'.format('*'*79), r.encoding)
+#r.encoding = 'GBK'
+#print(r.text, '\n{}\n'.format('*'*79), r.encoding)
+print r.encoding
