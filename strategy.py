a5e92f4d3e4c993f7e7e459decdd50eb0a2c05da
Arnold1974 <wangych_qd@163.com>
2017-01-25 17:12:42 +0800

    2017-01-25

--- strategy.py
+++ strategy.py
@@ -11,77 +11,6 @@ sys.stdin, sys.stdout, sys.stderr = stdi, stdo, stde  # 保持标准输入、标
 sys.setdefaultencoding('utf8')
 
 
-{
-"PICKSTOCK_URL":"http://www.iwencai.com/stockpick/load-data",
-"STRATEGY_URL":"http://www.iwencai.com/traceback/strategy/submit",
-"TRANSACTION_URL":"http://www.iwencai.com/traceback/strategy/transaction",
-"QUERY_4_DAYS":"非st; 收盘价在5元至30元之间; 总市值小于6000000000; 涨幅0%-6%; 15日区间涨跌幅<6%; 换手率<3.5%; 量比小于1.5; 市盈率(pe)<400;  boll突破中轨; dde大单净额流入; 一阳三线; a股市值(不含限售股)从小到大排列",
-"QUERY_2_DAYS":"DDE大单净量大于0.25；涨跌幅大于10%；市盈率小于45；非st股；非创业板；上市日期>30；总市值从小到大排列",
-
-"headers":{
-                "Host": "www.iwencai.com",
-                "Connection": "keep-alive",
-                "Accept": "application/json, text/javascript, */*; q=0.01",
-                "X-Requested-With": "XMLHttpRequest",
-                "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36",
-                "Referer": "http://www.iwencai.com",
-                "Accept-Encoding": "gzip, deflate, sdch",
-                "Accept-Language": "zh-CN,zh;q=0.8"
-                },
-
-"pickstock_params":{
-                "typed":"0",
-                "preParams":"",
-                "ts":"1",
-                "f":"1",
-                "qs":"result_original",
-                "selfsectsn":"",
-                "querytype":"",
-                "searchfilter":"",
-                "tid":"stockpick",
-                "w":"",
-                "queryarea":"" 
-                 },
-
-"traceback_params":{
-                "query":"",
-                "daysForSaleStrategy":"4",
-                "startDate":" ",
-                "endDate":" ",
-                "fell":"0.001",
-                "upperIncome":"20",
-                "lowerIncome":"8",
-                "fallIncome":"5",
-                "stockHoldCount":"1"
-                 },
-  
-"transaction_params":{
-                "stime":"2017-01-01",
-                "etime":"2027-10-20",
-                "hold_for":"4",
-                "sort":"desc",
-                "title":"bought_at",
-                "stockHoldCount":"1",
-                "fallIncome":"5",
-                "lowerIncome":"8",
-                "upperIncome":"20",
-                "fell":"0.001",
-                "endDate":" ",
-                "startDate":" ",
-                "daysForSaleStrategy":"4",
-                "query":"",
-                "newType":"0"
-                 },
-
-"income_control":{
-                "upperIncome":"20",
-                "lowerIncome":"8",
-                "fallIncome":"5"
-                }
-                 
-}
-
-
 class Strategy(object):
     """
     利用同花顺回测引擎获取策略所需股票.
@@ -97,7 +26,7 @@ class Strategy(object):
         self.upperIncome=str(upperIncome)
         self.lowerIncome=str(lowerIncome)
         self.fallIncome=str(fallIncome)
-        print "策略持股天数: %s days \t 止损止盈：%s|%s|%s" % (self.hold_days,self.upperIncome,self.lowerIncome,self.fallIncome)
+        print u"策略持股天数: %s days \t 止损止盈：%s|%s|%s" % (self.hold_days,self.upperIncome,self.lowerIncome,self.fallIncome)
         
     def pickstock(self):
         pickstock_params=self.config["pickstock_params"]
@@ -108,7 +37,7 @@ class Strategy(object):
 
 
     def traceback(self):
-        traceback_params=self.config["tracback_params"]
+        traceback_params=self.config["traceback_params"]
         traceback_params.update({"query":self.query,
                                 "daysForSaleStrategy":self.hold_days})
         traceback_params.update({"upperIncome":self.upperIncome,
@@ -168,7 +97,7 @@ if __name__=="__main__":
     test=Strategy("QUERY_4_DAYS")
     
     result=test.pickstock()
-    print "即时选股: %s \n" % result[0][1] if len(result)!=0 else "[]"
+    print u"即时选股: %s \n" % result[0][1] if len(result)!=0 else "[]"
     
     result= test.traceback()
     if result!=False:
