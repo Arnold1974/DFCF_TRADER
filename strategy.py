08594569606ac52cd03f1780ee6b2730f37e7ce9
Arnold1974 <wangych_qd@163.com>
2017-01-31 20:30:56 +0800

    2017/01/31

--- strategy.py
+++ strategy.py
@@ -99,34 +99,11 @@ class Strategy(object):
                     i+=1
             print u"%s 买入后, 应该卖出的第四个交易日为: %s" % (buy_date, df.ix[index,'calendarDate'])   
         test()
-
-class TradeCalendar(object):
-
-    def trade_calendar(self,buy_date,hold_days):
-        '''
-                交易日历
-        isOpen=1是交易日，isOpen=0为休市
-        '''
-        import pandas as pd
-        self.config=json.load(file("./config/strategy.json"))
-        df= pd.read_csv(self.config["TRADE_CALENDAR_URL"])
-        print df.ix[list(df['calendarDate']).index(buy_date),'isOpen']        
-        
-        #return pd.read_csv("http://218.244.146.57/static/calAll.csv")
-
-        index=df[df['calendarDate']==buy_date].index[0]
-        i=0
-        while i< int(hold_days-1):
-            index+=1
-            if df.ix[index,'isOpen'] == 1:
-                i+=1
-        print u"%s买入后, 应该卖出的第%d个交易日为: %s" % (buy_date, hold_days, df.ix[index, 'calendarDate'])   
-
-    
+ 
         
 if __name__=="__main__":
     test=Strategy("QUERY_4_DAYS")
-    
+   
     result=test.pickstock()
     print u"即时选股: %s \n" % (result[0][1] if len(result)!=0 else "[]")
     
--- trade_calendar.py
+++ trade_calendar.py
@@ -1,5 +1,6 @@
 #-*- coding:utf-8 -*-
 import pandas as pd
+import json,time
 
 class TradeCalendar(object):
 
@@ -8,11 +9,16 @@ class TradeCalendar(object):
                 交易日历
         isOpen=1是交易日，isOpen=0为休市
         '''
-
-        self.config=json.load(file("./config/strategy.json"))
-        df= pd.read_csv(self.config["TRADE_CALENDAR_URL"])
-        print df.ix[list(df['calendarDate']).index(buy_date),'isOpen']        
-        
+        test_day=time.strftime("%Y/%m/%d",time.localtime(time.time()+2550000))
+        df= pd.read_csv(".\calendar\calendar.csv")
+        #如果本地文件已过期，查不到数据，则网络更新
+        if not test_day in list(df['calendarDate']):
+            self.config=json.load(file("./config/strategy.json"))
+            df= pd.read_csv(self.config["TRADE_CALENDAR_URL"])
+            df.to_csv('.\calendar\calendar.csv')
+            print '### calendar.csv updated ###'
+            
+        print df.ix[list(df['calendarDate']).index(buy_date),'isOpen']              
         #return pd.read_csv("http://218.244.146.57/static/calAll.csv")
 
         index=df[df['calendarDate']==buy_date].index[0]
@@ -22,4 +28,8 @@ class TradeCalendar(object):
             if df.ix[index,'isOpen'] == 1:
                 i+=1
         print u"%s买入后, 应该卖出的第%d个交易日为: %s" % (buy_date, hold_days, df.ix[index, 'calendarDate'])   
-`
+
+        
+if __name__=="__main__":
+    a1=TradeCalendar()
+    a1.trade_calendar("2017/12/26",2)        
