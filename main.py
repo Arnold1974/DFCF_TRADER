669bb410b642224662633ba435024b33494dbe3b
Arnold1974 <wangych_qd@163.com>
2017-02-19 22:25:22 +0800

    2017/02/19

--- main.py
+++ main.py
@@ -241,13 +241,13 @@ def monitor_sell(code,buy_day,sell_day,stock_amount):
                          and time.localtime()[3:6]>=(14,59,45) and price_updated <> True \
                          and float(quotation.result['low'][0]) <> float(dfcf_quote['topprice'])  
         
-        # .3. 卖出日，非一字涨停，如果涨停价也不能触及止盈价，则涨到7%就卖出，不用等收盘
+        # .3. 卖出日，非一字涨停，如果涨停价也不能触及止盈价，则涨到 5% 就卖出，不用等收盘
         sell_condition_3=quotation.result['code'][0]==code and int(stock_amount)<>0 \
                          and sell_day==time.strftime("%Y/%m/%d",time.localtime(time.time())) \
                          and price_updated <> True \
                          and float(dfcf_quote['topprice']) < stop_sell_price \
                          and float(quotation.result['low'][0]) <> float(dfcf_quote['topprice']) \
-                         and float(quotation.result['price'][0]) >= float(dfcf_quote['topprice']) * 0.7
+                         and float(quotation.result['price'][0]) >= float(quotation.result['open'][0]) * 1.05
  
         if sell_condition_1 or sell_condition_2 or sell_condition_3:                
             quotation.show=0
